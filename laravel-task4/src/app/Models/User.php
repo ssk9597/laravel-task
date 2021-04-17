b9c6c592279c10f867b3a4772a7ced140f667157
sasaki <skyo.97.admj@gmail.com>
2021-04-17 21:48:29 +0900

    Models/Userを1つにまとめる

--- laravel-task4/src/app/Models/User.php
+++ laravel-task4/src/app/Models/User.php
@@ -2,10 +2,41 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class User extends Model
+class User extends Authenticatable
 {
+  use Notifiable;
+
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = [
+    'name', 'email', 'password',
+  ];
+
+  /**
+   * The attributes that should be hidden for arrays.
+   *
+   * @var array
+   */
+  protected $hidden = [
+    'password', 'remember_token',
+  ];
+
+  /**
+   * The attributes that should be cast to native types.
+   *
+   * @var array
+   */
+  protected $casts = [
+    'email_verified_at' => 'datetime',
+  ];
+
   // 1対多のリレーション（Users->親, Posts->子）
   public function posts()
   {
